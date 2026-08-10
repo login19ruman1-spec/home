1e236c2254b589939043b5ce2e4e5af15edbb05d
login19ruman1-spec <login19ruman1@gmail.com>
2026-08-10 12:06:56 +0300

    Update DomeGuardPlugin.java

--- src/main/java/ru/domeguard/DomeGuardPlugin.java
+++ src/main/java/ru/domeguard/DomeGuardPlugin.java
@@ -2,15 +2,32 @@ package ru.domeguard;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.domeguard.voidworld.VoidListener;
+import ru.domeguard.voidworld.VoidManager;
 
 public final class DomeGuardPlugin extends JavaPlugin {
     private DomeManager domeManager;
     private DamageManager damageManager;
     private CurseManager curseManager;
+    private VoidManager voidManager;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        if (getConfig().getBoolean("void.enabled", true)) {
+    voidManager = new VoidManager(this);
+    voidManager.createWorld();
+
+    double roofY = getConfig().getDouble(
+            "void.nether-roof-y",
+            127.0
+    );
+
+    Bukkit.getPluginManager().registerEvents(
+            new VoidListener(this, voidManager, roofY),
+            this
+    );
+}
         curseManager = new CurseManager(this);
         domeManager = new DomeManager(this);
         damageManager = new DamageManager(this, domeManager, curseManager);
@@ -20,7 +37,7 @@ public final class DomeGuardPlugin extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new RespawnListener(this), this);
         Bukkit.getPluginManager().registerEvents(new BoundaryListener(this, curseManager), this);
         damageManager.start();
-        getLogger().info("DomeGuard v1.4.0 enabled.");
+        getLogger().info("DomeGuard v1.5.0 enabled.");
     }
     @Override public void onDisable() { if (damageManager != null) damageManager.stop(); if (curseManager != null) curseManager.save(); }
 }
