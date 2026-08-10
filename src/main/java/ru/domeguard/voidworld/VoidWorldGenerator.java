46fe0f004e6d8ebcf2d1b541b590e69f7764e15c
login19ruman1-spec <login19ruman1@gmail.com>
2026-08-10 11:44:32 +0300

    Update VoidWorldGenerator.java

--- src/main/java/ru/domeguard/voidworld/VoidWorldGenerator.java
+++ src/main/java/ru/domeguard/voidworld/VoidWorldGenerator.java
@@ -1,1 +1,20 @@
+package ru.domeguard.voidworld;
 
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
+
+import java.util.Random;
+
+public final class VoidWorldGenerator extends ChunkGenerator {
+
+    @Override
+    public void generateNoise(
+            WorldInfo worldInfo,
+            Random random,
+            int chunkX,
+            int chunkZ,
+            ChunkData chunkData
+    ) {
+        // Полностью пустой мир.
+    }
+}
