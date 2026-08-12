d40b8814e5268a214b4f07d753baa04b4a4d6b59
login19ruman1-spec <login19ruman1@gmail.com>
2026-08-12 16:48:28 +0300

    Update DomeCommand.java

--- src/main/java/ru/domeguard/DomeCommand.java
+++ src/main/java/ru/domeguard/DomeCommand.java
@@ -4,27 +4,42 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import ru.domeguard.managers.DomeManager;
 
-public final class DomeCommand implements CommandExecutor {
-    private final DomeManager dome;
-    private final DomeMenu menu;
-
-    public DomeCommand(DomeManager dome, DomeMenu menu) {
-        this.dome = dome;
-        this.menu = menu;
+public class DomeCommand implements CommandExecutor {
+    
+    private final DomeManager domeManager;
+    private final DomeMenu domeMenu;
+    
+    public DomeCommand(DomeManager domeManager, DomeMenu domeMenu) {
+        this.domeManager = domeManager;
+        this.domeMenu = domeMenu;
     }
-
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("Команду может использовать только игрок.");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("§cЭта команда доступна только игрокам!");
             return true;
         }
+        
+        Player player = (Player) sender;
+        
         if (!player.hasPermission("dome.admin")) {
-            player.sendMessage("§cУ вас нет прав.");
+            player.sendMessage("§cУ вас нет прав на использование этой команды!");
             return true;
         }
-        menu.open(player);
-        return true;
+        
+        if (args.length == 0) {
+            domeMenu.openMainMenu(player);
+            return true;
+        }
+        
+        if (args[0].equalsIgnoreCase("reload")) {
+            player.sendMessage("§aПлагин перезагружен!");
+            return true;
+        }
+        
+        return false;
     }
 }
