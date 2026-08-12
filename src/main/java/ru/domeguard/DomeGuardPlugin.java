7e831750ca2381bc7ca67a474911fe696477a903
login19ruman1-spec <login19ruman1@gmail.com>
2026-08-12 16:28:45 +0300

    Update DomeGuardPlugin.java

--- src/main/java/ru/domeguard/DomeGuardPlugin.java
+++ src/main/java/ru/domeguard/DomeGuardPlugin.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import ru.domeguard.managers.CurseManager;
 import ru.domeguard.managers.DamageManager;
 import ru.domeguard.managers.DomeManager;
-import ru.domeguard.world.VoidWorldGenerator;
+import ru.domeguard.VoidWorldGenerator;
 
 public class DomeGuardPlugin extends JavaPlugin {
     
