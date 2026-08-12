3d810e38756b5541dfc74bb048ec89f013e78427
login19ruman1-spec <login19ruman1@gmail.com>
2026-08-12 16:13:25 +0300

    Update RespawnListener.java

--- src/main/java/ru/domeguard/RespawnListener.java
+++ src/main/java/ru/domeguard/RespawnListener.java
@@ -1,16 +1,16 @@
-package ваш.пакет.listeners;
+package ru.domeguard;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import ваш.пакет.managers.CurseManager; // замените на ваш менеджер
+import ru.domeguard.managers.CurseManager;
 
-public class PlayerRespawnListener implements Listener {
+public class RespawnListener implements Listener {
     
     private final CurseManager curseManager;
     
-    public PlayerRespawnListener(CurseManager curseManager) {
+    public RespawnListener(CurseManager curseManager) {
         this.curseManager = curseManager;
     }
     
@@ -18,15 +18,9 @@ public class PlayerRespawnListener implements Listener {
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         Player player = event.getPlayer();
         
-        // Проверяем, проклят ли игрок
         if (curseManager.isPlayerCursed(player.getUniqueId())) {
-            // Возвращаем все эффекты
             curseManager.applyCurseEffects(player);
-            
-            // Блокируем сон
             curseManager.blockSleep(player);
-            
-            // Отправляем сообщение
             player.sendMessage("§cВы всё ещё прокляты! Съешьте тушёный суп.");
         }
     }
