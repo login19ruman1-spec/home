ac346a2e7db8254d366cfcf40574ab959a591d6b
login19ruman1-spec <login19ruman1@gmail.com>
2026-08-12 16:01:25 +0300

    Update VoidWorldGenerator.java

--- src/main/java/ru/domeguard/voidworld/VoidWorldGenerator.java
+++ src/main/java/ru/domeguard/voidworld/VoidWorldGenerator.java
@@ -1,59 +1,28 @@
-package ru.domeguard.voidworld;
+package ваш.пакет.world;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.generator.WorldInfo;
-
 import java.util.Random;
 
-public final class VoidWorldGenerator extends ChunkGenerator {
-
+public class VoidWorldGenerator extends ChunkGenerator {
+    
     @Override
-    public void generateNoise(
-            WorldInfo worldInfo,
-            Random random,
-            int chunkX,
-            int chunkZ,
-            ChunkData chunkData
-    ) {
-        // Полностью пустой мир.
-        // Блоки создаются только самим VoidManager.
+    public void generateNoise(WorldInfo worldInfo, Random random, int x, int z, ChunkData chunkData) {
+        // Ничего не генерируем - мир полностью пустой
     }
-
+    
     @Override
-    public boolean shouldGenerateNoise() {
-        return false;
+    public void generateSurface(WorldInfo worldInfo, Random random, int x, int z, ChunkData chunkData) {
+        // Тоже пусто
     }
-
+    
     @Override
-    public boolean shouldGenerateSurface() {
-        return false;
-    }
-
-    @Override
-    public boolean shouldGenerateBedrock() {
-        return false;
-    }
-
-    @Override
-    public boolean shouldGenerateCaves() {
-        return false;
-    }
-
-    @Override
-    public boolean shouldGenerateDecorations() {
-        return false;
-    }
-
-    @Override
-    public boolean shouldGenerateMobs() {
-        return false;
-    }
-
-    @Override
-    public boolean shouldGenerateStructures() {
-        return false;
+    public Location getFixedSpawnLocation(World world, Random random) {
+        // Спавн в центре мира (0, 64, 0)
+        return new Location(world, 0, 64, 0);
     }
 }
-
 
 
