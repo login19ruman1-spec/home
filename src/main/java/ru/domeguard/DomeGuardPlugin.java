7523532e7e0f56dc1d9121dfd04b42a6b9d9bc44
login19ruman1-spec <login19ruman1@gmail.com>
2026-08-12 16:10:06 +0300

    Update DomeGuardPlugin.java

--- src/main/java/ru/domeguard/DomeGuardPlugin.java
+++ src/main/java/ru/domeguard/DomeGuardPlugin.java
@@ -1,43 +1,63 @@
 package ru.domeguard;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import ru.domeguard.voidworld.VoidListener;
-import ru.domeguard.voidworld.VoidManager;
+import ru.domeguard.listeners.BoundaryListener;
+import ru.domeguard.listeners.RespawnListener;
+import ru.domeguard.listeners.BorderPortalListener; // НОВЫЙ импорт
+import ru.domeguard.managers.Cursemanager;
+import ru.domeguard.managers.DamageManager;
+import ru.domeguard.managers.DomeManager;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import ru.domeguard.world.VoidWorldGenerator; // НОВЫЙ импорт
 
-public final class DomeGuardPlugin extends JavaPlugin {
-    private DomeManager domeManager;
+public class DomeGuardPlugin extends JavaPlugin {
+    
+    private Cursemanager curseManager;
     private DamageManager damageManager;
-    private CurseManager curseManager;
-    private VoidManager voidManager;
-
+    private DomeManager domeManager;
+    
     @Override
     public void onEnable() {
-        saveDefaultConfig();
-        if (getConfig().getBoolean("void.enabled", true)) {
-    voidManager = new VoidManager(this);
-    voidManager.createWorld();
-
-    double roofY = getConfig().getDouble(
-            "void.nether-roof-y",
-            127.0
-    );
-
-    Bukkit.getPluginManager().registerEvents(
-            new VoidListener(this, voidManager, roofY),
-            this
-    );
-}
-        curseManager = new CurseManager(this);
+        // --- 1. Инициализация менеджеров ---
+        curseManager = new Cursemanager(this);
+        damageManager = new DamageManager(this);
         domeManager = new DomeManager(this);
-        damageManager = new DamageManager(this, domeManager, curseManager);
-        DomeMenu menu = new DomeMenu(this, domeManager, damageManager);
-        getCommand("dome").setExecutor(new DomeCommand(domeManager, menu));
-        Bukkit.getPluginManager().registerEvents(menu, this);
-        Bukkit.getPluginManager().registerEvents(new RespawnListener(this), this);
-        Bukkit.getPluginManager().registerEvents(new BoundaryListener(this, curseManager), this);
-        damageManager.start();
-        getLogger().info("DomeGuard v1.5.0 enabled.");
+        
+        // --- 2. Регистрация команд ---
+        getCommand("dome").setExecutor(new DomeCommand(this));
+        
+        // --- 3. РЕГИСТРАЦИЯ СЛУШАТЕЛЕЙ (ВАЖНО!) ---
+        // Существующие слушатели
+        getServer().getPluginManager().registerEvents(new BoundaryListener(this), this);
+        getServer().getPluginManager().registerEvents(new RespawnListener(curseManager), this);
+        
+        // НОВЫЙ слушатель для портала на границе ада
+        getServer().getPluginManager().registerEvents(new BorderPortalListener(), this);
+        
+        // --- 4. Создание мира Void (если его нет) ---
+        createVoidWorld();
+        
+        getLogger().info("DomeGuard плагин успешно загружен!");
     }
-    @Override public void onDisable() { if (damageManager != null) damageManager.stop(); if (curseManager != null) curseManager.save(); }
+    
+    @Override
+    public void onDisable() {
+        getLogger().info("DomeGuard плагин выгружен.");
+    }
+    
+    // Метод для создания мира Void
+    private void createVoidWorld() {
+        World voidWorld = getServer().getWorld("void_world");
+        if (voidWorld == null) {
+            getLogger().info("Создание мира Void...");
+            WorldCreator creator = new WorldCreator("void_world");
+            creator.environment(World.Environment.NORMAL);
+            creator.generator(new VoidWorldGenerator());
+            creator.createWorld();
+            getLogger().info("Мир Void создан!");
+        } else {
+            getLogger().info("Мир Void уже существует.");
+        }
+    }
 }
